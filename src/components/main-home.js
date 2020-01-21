fc981b47fc16bb0b3d2e39e7bb64139b25ff0086
SGordijn <35259753+SGordijn@users.noreply.github.com>
2020-01-21 21:15:58 +0100

    Final Version
    Last bits of code to complete the website!

--- src/components/main-home.js
+++ src/components/main-home.js
@@ -8,26 +8,30 @@ export class MainHome extends React.Component {
           <article className="article-title flex-row-around flex-align-items-center" data-aos="fade-down" data-aos-duration="1500">
             <h1 className="main-title">Welcome</h1>
           </article>
+
           <article className="article-half flex-row-between flex-wrap flex-align-items-center">
-            <div className="half-widget flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
+            <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">Projects</h2>
               <p className="widget-text center-text">In this page, you will get to see a small collection of projects that I have worked on and/or completed.</p>
-              <a className="btn btn-orange" href="/project">Click Here</a>
+              <a className="btn btn-orange btn-shadow" href="/project">Click Here</a>
             </div>
-            <div className="half-widget flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
+
+            <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">Team Projectile</h2>
               <p className="widget-text center-text">Want to know more about the group of YouTubers and Twitch streamers that I am part of? This is the place to read all about it!</p>
-              <a className="btn btn-orange" href="/team">Click Here</a>
+              <a className="btn btn-orange btn-shadow" href="/team">Click Here</a>
             </div>
-            <div className="half-widget flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
+
+            <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">LinkedIn</h2>
               <p className="widget-text center-text">Do you want to see my skills in one place? Check out my profile on LinkedIn!</p>
-              <a className="btn btn-orange" href="https://www.linkedin.com/in/stephan-gordijn-062636171/" target="_blank" rel="noopener noreferrer">Click Here</a>
+              <a className="btn btn-orange btn-shadow" href="https://www.linkedin.com/in/stephan-gordijn-062636171/" target="_blank" rel="noopener noreferrer">Click Here</a>
             </div>
-            <div className="half-widget flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
+
+            <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">GitHub</h2>
               <p className="widget-text center-text">This is the place where I put my code in, including the code of this very website. If you have suggestions for websites or code, let me know!</p>
-              <a className="btn btn-orange" href="https://github.com/SGordijn" target="_blank" rel="noopener noreferrer">Click Here</a>
+              <a className="btn btn-orange btn-shadow" href="https://github.com/SGordijn" target="_blank" rel="noopener noreferrer">Click Here</a>
             </div>
           </article>
         </section>
--- src/components/top-header.js
+++ src/components/top-header.js
@@ -4,7 +4,7 @@ export class TopHeader extends React.Component {
   render() {
     return (
       <div className="top-header flex-row-between flex-nowrap flex-align-items-center">
-        <p>eclipseprojectile@gmail.com</p>
+        <p className="top-title">Stephan Gordijn</p>
         <ul className="top-links flex-row-between">
           <li><a href="https://github.com/SGordijn" target="_blank" rel="noopener noreferrer"><img className="top-social-img" alt="GitHub" src="img/github.png"/></a></li>
           <li><a href="https://www.linkedin.com/in/stephan-gordijn-062636171/" target="_blank" rel="noopener noreferrer"><img className="top-social-img" alt="LinkedIn" src="img/linkedin.png"/></a></li>
