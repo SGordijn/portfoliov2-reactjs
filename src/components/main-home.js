6e45abccc0d9d97d3b2ac1fc6d2de02cd5932086
SGordijn <35259753+SGordijn@users.noreply.github.com>
2020-09-22 06:49:43 +0200

    Background Color Adjustments

--- src/components/main-home.js
+++ src/components/main-home.js
@@ -13,25 +13,25 @@ export class MainHome extends React.Component {
             <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">Projects</h2>
               <p className="widget-text center-text">In this page, you will get to see a small collection of projects that I have worked on.</p>
-              <a className="btn btn-orange btn-shadow" href="/project">Click Here</a>
+              <a className="btn btn-black btn-shadow" href="/project">Click Here</a>
             </div>
 
             <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">Friends</h2>
               <p className="widget-text center-text">From former colleagues to fellow streamers, you will find a collection of fantastic people I've had the pleasure of working with in this page.</p>
-              <a className="btn btn-orange btn-shadow" href="/friends">Click Here</a>
+              <a className="btn btn-black btn-shadow" href="/friends">Click Here</a>
             </div>
 
             <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">LinkedIn</h2>
               <p className="widget-text center-text">Do you want to see my skills in one place? Check out my profile on LinkedIn!</p>
-              <a className="btn btn-orange btn-shadow" href="https://www.linkedin.com/in/stephan-gordijn-062636171/" target="_blank" rel="noopener noreferrer">Click Here</a>
+              <a className="btn btn-black btn-shadow" href="https://www.linkedin.com/in/stephan-gordijn-062636171/" target="_blank" rel="noopener noreferrer">Click Here</a>
             </div>
 
             <div className="half-widget home-size flex-column-between flex-wrap flex-align-items-center" data-aos="zoom-in" data-aos-duration="1500">
               <h2 className="widget-title">GitHub</h2>
               <p className="widget-text center-text">This is the place where I put my code in, including the code of this very website. If you have suggestions for websites or code, let me know!</p>
-              <a className="btn btn-orange btn-shadow" href="https://github.com/SGordijn" target="_blank" rel="noopener noreferrer">Click Here</a>
+              <a className="btn btn-black btn-shadow" href="https://github.com/SGordijn" target="_blank" rel="noopener noreferrer">Click Here</a>
             </div>
           </article>
         </section>
